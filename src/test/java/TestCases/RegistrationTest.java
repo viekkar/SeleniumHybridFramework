e7dc9bb97ebe0a638a64a3a50ccfb8bc6c4c6d36
Your Name <viekkar@gmail.com>
2025-01-12 01:06:43 +0530

    Second commit

--- src/test/java/TestCases/RegistrationTest.java
+++ src/test/java/TestCases/RegistrationTest.java
@@ -11,9 +11,12 @@ public class RegistrationTest extends Base {
 
     @Test
     public void verifyRegistration(){
+        logger.info("**********Starting Resgistration Test***********");
         HomePage homePage=new HomePage(driver);
         homePage.clickMyAccount();
+        logger.info("Clicked on My account");
         homePage.clickRegister();
+        logger.info("Clicked on Registration");
         RegistrationPage registrationPage =new RegistrationPage(driver);
         registrationPage.setFirstName(getRandomString().toUpperCase());
         registrationPage.setLastName(getRandomString().toUpperCase());
@@ -24,6 +27,7 @@ public class RegistrationTest extends Base {
         registrationPage.setmyConfirmation(password);
         registrationPage.checkBox();
         registrationPage.submit();
+        logger.info("Submitted registration");
         Assert.assertEquals("Your Account Has Been Created!",registrationPage.getConfirmationMsg());
     }
 
--- src/test/java/testBase/Base.java
+++ src/test/java/testBase/Base.java
@@ -1,6 +1,8 @@
 package testBase;
 
 import org.apache.commons.lang3.RandomStringUtils;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterClass;
@@ -8,10 +10,13 @@ import org.testng.annotations.BeforeClass;
 
 import java.time.Duration;
 
+
 public class Base {
     public WebDriver driver;
+    public Logger logger;
     @BeforeClass
     public void setup(){
+        logger= LogManager.getLogger();
         driver= new ChromeDriver();
         driver.manage().deleteAllCookies();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
